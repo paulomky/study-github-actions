c4db97b867f02e42cf27eb7255cb3b5628f11dba
Paulo Oliveira <pauloeduardo2001@hotmail.com>
2021-11-08 16:52:25 -0300

    teste push

--- dist/index.js
+++ dist/index.js
@@ -8455,6 +8455,7 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
+  
   core.setFailed(error.message);
 }
 })();
